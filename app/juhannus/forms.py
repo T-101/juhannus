1f3f23b48f4803789c10f942edb8ab40ac87657e
Antti Rummukainen <t-101@t-101.org>
2019-05-06 23:06:08 +0300

    added validation to check that name is available

--- app/juhannus/forms.py
+++ app/juhannus/forms.py
@@ -4,6 +4,16 @@ from juhannus.models import Participant
 
 
 class SubmitForm(forms.ModelForm):
+
+    def clean(self):
+        if self.data.get("action") == "delete":
+            return self.cleaned_data
+
+        name = self.cleaned_data.get("name")
+        event = self.cleaned_data.get("event")
+        if event.get_participants().filter(name__iexact=name.strip()):
+            raise forms.ValidationError({"name": "Name already in use. Choose another"})
+
     class Meta:
         model = Participant
         fields = ('name', 'vote', 'event')
@@ -23,14 +33,3 @@ class SubmitForm(forms.ModelForm):
                 'min': 0,
                 'max': 100})
         }
-        error_messages = {
-            'handle': {
-                'required': 'Tää sun handle olis vähän niinku tarpeellinen tieto.',
-                'invalid': 'Ai sä meinasit ettei bäkkäri validoi näitä kans?',
-                'unique': 'Näin lyhyt handlelista ja silti osuit dupeen. Yritä ny ees.'
-            },
-            'amount': {
-                'min_value': '0-100 kelpaa. Kyl sä pystyt siihen.',
-                'max_value': '0-100 kelpaa. Kyl sä pystyt siihen.',
-                'invalid': 'Ai sä meinasit ettei bäkkäri validoi näitä kans? 0-100 plx'
-            }}
